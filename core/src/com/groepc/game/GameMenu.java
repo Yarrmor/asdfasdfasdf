c7266d6642dfa21673c60aa8537f2fc89a8f4752
Boer,Bram B.P. de <bram.deboer@student.fontys.nl>
2016-03-02 13:28:54 +0100

    menu met paar knoppen werkt

--- core/src/com/groepc/game/GameMenu.java
+++ core/src/com/groepc/game/GameMenu.java
@@ -8,6 +8,8 @@ package com.groepc.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -15,42 +17,42 @@ import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-public final class GameMenu implements Screen {
+public class GameMenu extends ApplicationAdapter implements InputProcessor {
 
-    Skin skin;
-    Stage stage;
-    SpriteBatch batch;
-    Game g;
+    private Skin skin;
+    private Stage stage;
 
-    public GameMenu(Game g) {
-        create();
-        this.g = g;
-    }
+    private Table table;
+    private TextButton startButton;
+    private TextButton quitButton;
+    
+    private final int ButtonHeight = 150;
+    private final int ButtonWidth = 50;
 
-    public GameMenu() {
-        stage = new Stage();
-        skin = new Skin();
-        batch = new SpriteBatch();
-        Gdx.input.setInputProcessor(stage);
-        create();
-    }
+    private SpriteBatch batch;
+    private Sprite sprite;
 
+    @Override
     public void create() {
-        // A skin can be loaded via JSON or defined programmatically, either is fine. Using a skin is optional but strongly
-        // recommended solely for the convenience of getting a texture, region, etc as a drawable, tinted drawable, etc.
-        
-        // Generate a 1x1 white texture and store it in the skin named "white".
-        Pixmap pixmap = new Pixmap(100, 100, Format.RGBA8888);
+        skin = new Skin();
+        Pixmap pixmap = new Pixmap(ButtonHeight, ButtonWidth, Format.RGBA8888);
         pixmap.setColor(Color.GREEN);
         pixmap.fill();
 
@@ -71,69 +73,106 @@ public final class GameMenu implements Screen {
 
         skin.add("default", textButtonStyle);
 
-        // Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
-        final TextButton textButton = new TextButton("PLAY", textButtonStyle);
-        textButton.setPosition(200, 200);
-        stage.addActor(textButton);
-        stage.addActor(textButton);
-        stage.addActor(textButton);
+        stage = new Stage(new ScreenViewport());
+        table = new Table();
+        table.setWidth(stage.getWidth());
+        table.align(Align.center | Align.top);
 
-        // Add a listener to the button. ChangeListener is fired when the button's checked state changes, eg when clicked,
-        // Button#setChecked() is called, via a key press, etc. If the event.cancel() is called, the checked state will be reverted.
-        // ClickListener could have been used, but would only fire when clicked. Also, canceling a ClickListener event won't
-        // revert the checked state.
-        textButton.addListener(new ChangeListener() {
+        table.setPosition(0, Gdx.graphics.getHeight());
+        startButton = new TextButton("New Game", skin);
+        quitButton = new TextButton("Quit Game", skin);
+
+        startButton.addListener(new ClickListener() {
             @Override
-            public void changed(ChangeListener.ChangeEvent event, Actor actor) {
-                //System.out.println("Clicked! Is checked: " + button.isChecked());
-                textButton.setText("Starting new game");
-                g.setScreen(new GameMenu());
-
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log("Clicked button", "Start");
+                event.stop();
             }
         });
+        quitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log("Clicked button", "Quit");
+                event.stop();
+            }
+        });
+        
+        table.padTop(30);
+
+        table.add(startButton).padBottom(30);
+
+        table.row();
+        table.add(quitButton);
+
+        stage.addActor(table);
+
+        batch = new SpriteBatch();
+        sprite = new Sprite(new Texture(Gdx.files.internal("badlogic.jpg")));
+        sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                sprite.setFlip(false, !sprite.isFlipY());
+            }
+        }, 10, 10, 10000);
+
+        // ORDER IS IMPORTANT!
+        InputMultiplexer im = new InputMultiplexer(stage, this);
+        Gdx.input.setInputProcessor(im);
     }
 
     @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+
+        batch.begin();
+        sprite.draw(batch);
+        batch.end();
+
+        stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
-        stage.setDebugAll(true);
     }
 
     @Override
-    public void resize(int width, int height) {
-        stage.setViewport(new StretchViewport(width, height));
+    public boolean keyDown(int keycode) {
+        return false;
     }
 
     @Override
-    public void dispose() {
-        stage.dispose();
-        skin.dispose();
+    public boolean keyUp(int keycode) {
+        return false;
     }
 
     @Override
-    public void show() {
-        // TODO Auto-generated method stub
-
+    public boolean keyTyped(char character) {
+        return false;
     }
 
     @Override
-    public void hide() {
-        // TODO Auto-generated method stub
-
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        sprite.setFlip(!sprite.isFlipX(), sprite.isFlipY());
+        return true;
     }
 
     @Override
-    public void pause() {
-        // TODO Auto-generated method stub
-
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
     }
 
     @Override
-    public void resume() {
-        // TODO Auto-generated method stub
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
 
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
